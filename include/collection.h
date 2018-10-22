68da25bdb4590bdc64cc31661f1f34c5467d67c3
Abdulmalek Al-Gahmi <aalgahmi@abdulmaleks-mbp.wireless.weber.edu>
2018-10-22 07:21:50 -0600

    Initial commit

---
+++ include/collection.h
@@ -0,0 +1,76 @@
+#ifndef _COLLECTION_H_
+#define _COLLECTION_H_
+
+#include <iostream>
+template<typename Item>
+class Collection {
+public:
+    Collection(): sz(0), capacity(8), elements(new Item[capacity]){}
+    Collection(int size): sz(size), capacity(size), elements(new Item[size]){}
+    Collection(const Collection<Item>& c): sz(c.sz), capacity(c.capacity) {
+        elements = new Item[capacity];
+        for(int i = 0; i < c.sz; i++){
+            elements[i] = c.elements[i];
+        }
+    }
+
+    Collection<Item>& operator=(const Collection<Item>& c){
+       Item* newElements = new Item[c.capacity];
+       for(int i = 0; i < sz; i++){
+             newElements[i] = c.elements[i];
+       }
+       
+       delete[] elements;
+       elements = newElements;
+       sz = c.sz;
+       capacity = c.capacity;
+
+       return *this;
+    }
+
+    // Required to support range-based for loop
+    Item* begin(){ return elements; }
+    Item* end(){ return elements + sz; }
+
+    void add(Item e){
+        if(sz == capacity){
+            Item* newElements = new Item[capacity * 2];
+            for(int i = 0; i < sz; i++){
+                newElements[i] = elements[i];
+            }
+            delete[] elements;
+            elements = newElements;
+            capacity *= 2;
+        }
+        elements[sz++] = e;
+    }
+
+    Item operator[](int i) const{
+        return elements[i];
+    }
+
+    Item& operator[](int i) {
+        return elements[i];
+    }
+    
+    Collection<Item>& remove(){
+        sz--;
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Collection<Item>& c){
+        for(int i = 0; i < c.sz; i++){
+            out << c.elements[i] << " ";
+        }
+
+        return out;
+    }
+
+    ~Collection(){
+        delete[] elements;
+    }
+protected:
+    int  sz, capacity;
+    Item *elements;
+};
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+#include "collection.h"
+#include "linked_list.h"
+using namespace std;
+
+int main(){
+  //TODO
+  return 0;
+}
